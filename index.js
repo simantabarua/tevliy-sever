b05c1a91a9a2504f3e8ed0c1669b894cfff055c8
simantabarua <webdevelopersimanta@gmail.com>
2023-04-28 12:21:37 +0600

    updat

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// console.log(uri);
-
 
 async function run() {
     try {
